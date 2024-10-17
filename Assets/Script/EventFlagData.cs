2fd9fc821aea31534def4d3221933787c0eb6d91
HIRATA05 <SBA22G030007@edu.sba.ac.jp>
2024-10-18 04:25:40 +0900

    実験のための画像取り込みなど

---
+++ Assets/Script/EventFlagData.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EventFlag", menuName = "ScriptableObjects/CreateEventFlagData")]
+public class EventFlagData : ScriptableObject
+{
+    //シナリオ進行のためのフラグ設定
+
+
+
+}
