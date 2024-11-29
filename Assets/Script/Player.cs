b9574358bfc233d94cc9f841a1532ef7e3e88910
Kamiyashiki <SBA22G030002@edu.sba.ac.jp>
2024-11-29 18:01:07 +0900

    マップ遷移シーン追加忘れ

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -7,22 +7,50 @@ public class Player : MonoBehaviour
     [SerializeField] private RoomController roomController;
 
     bool isViewMap = false;
+    int mapLayer;
+    int defaultLayer;
+
+    private void Start()
+    {
+        defaultLayer = 1 << LayerMask.NameToLayer("Default");
+        mapLayer = 1 << LayerMask.NameToLayer("UI");
+        Camera.main.cullingMask = defaultLayer;
+    }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.M))
         {
             // マップを開く
-            isViewMap = true;
+            isViewMap = (isViewMap == true) ? false : true;
         }
 
         if (isViewMap)
         {
-            if (Input.GetKeyDown(KeyCode.Q))
+            Camera.main.cullingMask = mapLayer;
+
+            float DebugDrawRayDistance = 15.0f;
+            if (Input.GetMouseButtonDown(0))
             {
-                // enum 型で部屋名を指定
-                roomController.MoveRoom(RoomNames.LivingRoom, transform);
+                var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out var hit))
+                {
+                    string objectName = hit.collider.gameObject.name;
+                    Debug.Log("左クリック:" + objectName);
+
+                    if (hit.collider.transform.name == RoomNames.Kitchen.ToString())
+                    {
+                        roomController.MoveRoom(RoomNames.Kitchen, transform);
+                        isViewMap = false;
+                    }
+                }
+                Debug.DrawRay(ray.origin, ray.direction * DebugDrawRayDistance, Color.green, 5, false);
             }
+        }
+        else
+        {
+            Camera.main.cullingMask = defaultLayer;
+            isViewMap = false;
         }
     }
 }
